e59214e03e65416cfa55068bdc94a0ba219be8b7
Liam Hunt <29338686+Liam-Hunt@users.noreply.github.com>
2022-03-22 20:54:08 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, FALSE)
-curl -sSL https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh > dotnet-install.sh
+curl https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
 ./dotnet6/dotnet --version
